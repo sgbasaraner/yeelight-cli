78f2c82fff3cc89f9ac02b5d111c60e6a1d8bbab
Sarp Basaraner <sarpbasaraner@gmail.com>
2018-03-07 13:11:46 +0300

    separate bulb detection

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,20 @@ fn main() {
     };
 
     send_search_broadcast(&socket);
+    detect_bulbs(&socket);
+}
 
+fn send_search_broadcast(socket: &UdpSocket) {
+    let message = 
+                    "M-SEARCH * HTTP/1.1\r\n
+                    HOST: 239.255.255.250:1982\r\n
+                    MAN: \"ssdp:discover\"\r\n
+                    ST: wifi_bulb".as_bytes();
+
+    socket.send_to(message, MULTICAST_ADDR).expect("couldn't send to socket");
+}
+
+fn detect_bulbs(socket: &UdpSocket) {
     let mut buf = [0; 2048];
     loop {
         match socket.recv_from(&mut buf) {
@@ -27,14 +40,4 @@ fn main() {
             }
         }
     }
-}
-
-fn send_search_broadcast(socket: &UdpSocket) {
-    let message = 
-                    "M-SEARCH * HTTP/1.1\r\n
-                    HOST: 239.255.255.250:1982\r\n
-                    MAN: \"ssdp:discover\"\r\n
-                    ST: wifi_bulb".as_bytes();
-
-    socket.send_to(message, MULTICAST_ADDR).expect("couldn't send to socket");
 }
