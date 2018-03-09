1fdf31ba73e35c278a6dd2307594e6c866cb6d82
Sarp Basaraner <sarpbasaraner@gmail.com>
2018-03-09 08:34:31 +0300

    remove socket creation from main fn

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
 use std::str;
-use std::thread;
+use std::{thread, time};
 use std::net::UdpSocket;
 
 const MULTICAST_ADDR: &'static str = "239.255.255.250:1982";
 
 fn main() {
-    let socket = match UdpSocket::bind("0.0.0.0:34254") {
-        Ok(s) => s,
-        Err(e) => panic!("couldn't bind socket: {}", e)
-    };
-
-    send_search_broadcast(&socket);
+    let socket = create_socket();
     detect_bulbs(&socket);
 }
 
@@ -26,22 +21,41 @@ fn send_search_broadcast(socket: &UdpSocket) {
 
 fn detect_bulbs(socket: &UdpSocket) {
     let mut buf = [0; 2048];
+    let mut time_elapsed: i32 = 0;
+    let search_interval: i32 = 300;
+    let read_interval: i32 = 1;
+
     loop {
+        if time_elapsed % search_interval == 0 {
+            send_search_broadcast(socket);
+        }
         match socket.recv_from(&mut buf) {
             Ok((amt, src)) => {
                 thread::spawn(move || {
                     println!("amt: {}", amt);
                     println!("src: {}", src);
                 });
-                process_search_response(str::from_utf8(&buf).unwrap_or(""))
+                process_search_response(str::from_utf8(&buf).unwrap_or(""));
+                let sleep_time = time::Duration::from_millis((read_interval as f32 / 10.0) as u64 * 1000);
+                thread::sleep(sleep_time);
             },
             Err(e) => {
-                println!("couldn't receive a datagram: {}", e);
+                println!("couldn't receive a response: {}", e);
+                break;
             }
         }
+        time_elapsed += read_interval;
+        println!("{}", time_elapsed);
     }
 }
 
 fn process_search_response(response: &str) {
     println!("{}", response);
+}
+
+fn create_socket() -> UdpSocket {
+    match UdpSocket::bind("0.0.0.0:34254") {
+        Ok(s) => return s,
+        Err(e) => panic!("couldn't bind socket: {}", e)
+    };
 }
